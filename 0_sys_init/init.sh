b69cfde8255b42d23cdf4e3b7f380cb364e26071
luomgf <luomgf@163.com>
2016-06-24 01:50:26 +0800

    增加添加动态库功能

--- 0_sys_init/init.sh
+++ 0_sys_init/init.sh
@@ -16,7 +16,13 @@ init_selinux(){
 init_ldconf(){
 	grep -q '/usr/local/lib' /etc/ld.so.conf       || sed '$a /usr/local/lib ' /etc/ld.so.conf
 	grep -q '/usr/lib64/' /etc/ld.so.conf          || sed '$a /usr/lib64/'  /etc/ld.so.conf
-
+	for app in $(ls /usr/local)
+	do	
+	[ -d /usr/local/$app/lib ] || continue
+	grep  "/usr/local/$app/lib"  /etc/ld.so.conf.d/luomgf.conf  && continue
+	echo "/usr/local/$app/lib" >> /etc/ld.so.conf.d/luomgf.conf
+	
+	done
 	ldconfig  -v
 }
 	init_iptables
