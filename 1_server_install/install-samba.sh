e4686cb86768fa0fd4974abeaa5d200333fbec30
luomgf <luomgf@163.com>
2016-06-24 21:31:58 +0800

    修改samba安装脚本

--- 1_server_install/install-samba.sh
+++ 1_server_install/install-samba.sh
@@ -4,6 +4,8 @@ PJPWD=$PWD
 PKGVERSION=4.4.3
 PKGNAME=samba-4.4.3.tar.gz
 cd ~/pkg
+
+local_install(){
 [ -f $PKGNAME ] || wget -r -np -nd --accept=gz --no-check-certificate  https://download.samba.org/pub/samba/stable/samba-4.4.3.tar.gz
 [ -d  samba-$PKGVERSION ] || tar -zxvf $PKGNAME
 
@@ -20,7 +22,7 @@ cp   ./packaging/RHEL/setup/smb.init   /etc/init.d/smb
 cp   ./examples/tridge/smb.conf   /usr/local/samba/etc
 
 ulimit  -n 2048
-
+}
 fChangeConf(){
     grep -q 'nofile' ||
      sed  -i  '$a root      -      nofile       16384' /etc/security/limits.conf
@@ -29,7 +31,13 @@ fChangeConf(){
 fAdduser(){
 mkdir  -p /etc/samba
 [ -f  /etc/samba/smbpasswd  ] || touch /etc/samba/smbpasswd
-/usr/local/samba/bin/smbpasswd  -a root
+/usr/local/samba/bin/smbpasswd  -a code
 }
 cd $PJPWD
 
+install_byyum(){
+	yum install samba* samba-common-* samba-client*  -y
+	smbpasswd  -a code
+}
+
+	install_byyum
