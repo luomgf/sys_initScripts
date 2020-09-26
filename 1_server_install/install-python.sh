acc72f7ee158a044f858435429e6bc553c0b397e
luomgf <luomgf@163.com>
2020-09-25 22:10:07 -0400

    修改pythong安装

--- 1_server_install/install-python.sh
+++ 1_server_install/install-python.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VER="3.5.2"
+VER="3.7.0rc1"
 PKGNAME="Python-$VER.tgz"
 PKGDIR=${PKGNAME%.tgz}
 PREFIX="/opt/python/$PKGDIR"
@@ -8,39 +8,43 @@ SRCDIR=~/sys_scripts/1_server_install
 PKGHOME="~/pkg/tmp/$PKGDIR"
 
 install_init(){
-	mkdir  -p  $PKGHOME
-	cd ~/pkg
+        mkdir  -p  $PKGHOME
+        cd ~/pkg
 }
 install_yum(){
-	yum install -y  openssl*    sqlite* 
+        yum install -y  openssl*    sqlite* 
 }
 
 install_python(){
 #detail version: 2.7.9
 PYVER=${1-"3.4.4"}
 
-	cd ~/pkg
-	[ -f $PKGNAME ]  ||  
-	curl  -O  https://www.python.org/ftp/python/${PYVER}/Python-${PYVER}.tgz
-	cp $PKGNAME  ~/pkg/tmp/
-	cd ~/pkg/tmp/
-	[ -d $PKGHOME ] || tar  -zxf  $PKGNAME
- 	cd  $PKGDIR
-	./configure   --prefix=$PREFIX  \
-		--enable-shared        \
- 		--enable-loadable-sqlite-extensions  \
- 		--enable-ipv6
-	make  && make install
-	rm  -rf /usr/bin/python
-	ln  -s $PREFIX/bin/python${PPYVER}   /usr/bin/python 
-	sed    -i   's~#!/usr/bin/python$~#!/usr/bin/python2.7~'        /usr/bin/yum
-	grep -q  "$PREFIX/bin/"  /etc/profile      || 
-	sed  -i   '$a PATH='$PREFIX'/bin/:$PATH ; export  PATH ' /etc/profile 
-	[ -L /usr/local/python ] && rm  -rf  /usr/local/python
+        cd ~/pkg
+        [ -f $PKGNAME ]  ||  
+        curl  -O  https://www.python.org/ftp/python/${PYVER}/Python-${PYVER}.tgz
+        cp $PKGNAME  ~/pkg/tmp/
+        cd ~/pkg/tmp/
+        [ -d $PKGHOME ] || tar  -zxf  $PKGNAME
+        cd  $PKGDIR
+        ./configure   --prefix=$PREFIX  \
+                --enable-shared        \
+                --enable-loadable-sqlite-extensions  \
+                --enable-ipv6
+        make  && make install
+        }
+
+
+change_ini(){
+rm  -rf /usr/bin/python
+        ln  -s $PREFIX/bin/python${PPYVER}   /usr/bin/python 
+        sed    -i   's~#!/usr/bin/python$~#!/usr/bin/python2.7~'        /usr/bin/yum
+        grep -q  "$PREFIX/bin/"  /etc/profile      || 
+        sed  -i   '$a PATH='$PREFIX'/bin/:$PATH ; export  PATH ' /etc/profile 
+        [ -L /usr/local/python ] && rm  -rf  /usr/local/python
         ln -s  $PREFIX  /usr/local/python
-	python  -V
+        python  -V
 }
 
 install_yum
-install_python  3.5.2
-
+install_python  $VER
+#change_ini
